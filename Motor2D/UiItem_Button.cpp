da7a4e1a7c1bd0f738f1c4a9adc28d72c3a0dab2
Pol <36154612+PolRecasensSarra@users.noreply.github.com>
2019-03-17 12:56:00 +0100

    Added button class

---
+++ Motor2D/UiItem_Button.cpp
@@ -0,0 +1,32 @@
+
+#include "UiItem_Button.h"
+#include "j1App.h"
+#include "j1Render.h"
+#include "j1Gui.h"
+#include "p2Point.h"
+#include "j1Scene.h"
+#include "j1Map.h"
+
+UiItem_Button::UiItem_Button(SDL_Rect hitBox, const SDL_Rect * idle, UiItem * const parent, const SDL_Rect * click, const SDL_Rect * hover, p2Point<int> pivot) : UiItem(hitBox, parent, pivot)
+{
+}
+
+void UiItem_Button::AddFuntion(void(*funtionOnClick)(), bool Down)
+{
+}
+
+void UiItem_Button::AddFuntion(std::string & string, bool Down)
+{
+}
+
+void UiItem_Button::Draw(const float &)
+{
+}
+
+void UiItem_Button::OnClickUp()
+{
+}
+
+void UiItem_Button::OnClickDown()
+{
+}
---
+++ Motor2D/UiItem_Button.h
@@ -0,0 +1,25 @@
+#ifndef UIITEM_BUTTON_H
+#define UIITEM_BUTTON_H
+#include "UiItem.h"
+#include "p2Point.h"
+#include "j1Textures.h"
+#include <string>
+
+class UiItem_Button :public UiItem
+{
+protected:
+	SDL_Rect frames[MAX_STATES];
+	void(*funtionOnClick) () = nullptr;
+	void(*funtionOnUp)() = nullptr;
+
+public:
+	UiItem_Button(SDL_Rect hitBox, const SDL_Rect * idle, UiItem* const parent, const SDL_Rect * click = nullptr, const SDL_Rect * hover = nullptr, p2Point<int> pivot = { 0,0 });
+	void AddFuntion(void(*funtionOnClick) (), bool Down = true);
+	void AddFuntion(std::string & string, bool Down = true);
+
+	void Draw(const float&);
+	void OnClickUp();
+	void OnClickDown();
+};
+
+#endif
