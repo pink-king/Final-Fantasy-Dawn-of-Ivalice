217332d82568f053d6a70d5387a0d5ab407749ea
Gerard Clotet Rodriguez <36155703+GerardClotet@users.noreply.github.com>
2019-04-03 18:51:35 +0200

    Revert "Master into dev"

--- Motor2D/j1App.h
+++ Motor2D/j1App.h
@@ -23,9 +23,6 @@ class j1EntityFactory;
 class j1PathFinding;
 class j1Fonts;
 class j1BuffManager;
-class UiItem_HitPointManager;
-
-
 class j1AttackManager;
 class j1LootSystem;
 class j1App
@@ -111,9 +108,6 @@ public:
 	j1PathFinding*		pathfinding = nullptr; 
 	j1Fonts*			font = nullptr;
 	j1BuffManager*		buff = nullptr;
-	UiItem_HitPointManager*   HPManager = nullptr; 
-
-
 	j1AttackManager*	attackManager = nullptr;
 	j1LootSystem*		loot = nullptr;
 	bool				vsync = false;
