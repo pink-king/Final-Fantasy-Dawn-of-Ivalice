6ac00e6eb8c060b4217b63a50828d0fdb311a5bb
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2019-04-03 13:52:43 +0200

    erase item crashes the timer

--- Motor2D/UiItem_HitPointManager.cpp
+++ Motor2D/UiItem_HitPointManager.cpp
@@ -85,13 +85,13 @@ void UiItem_HitPointManager::DestroyHitPointLabel(UiItem_HitPoint* label)
 					label->texture = nullptr; 
 				}
 
-				// IMPORTANT!!: do this only if it is not being deleted already in the destroy element function in gui cpp
+				// this is supposdely not necessary 
 				/*delete *item;
 				*item = nullptr;*/
 			
 				// delete it from the hitPoint item list
 
-				hitPointLabels.erase(item); 
+				hitPointLabels.erase(item);              // if I do this, timer crashes
 				
 				break;
 				
@@ -118,10 +118,10 @@ void UiItem_HitPointManager::callHPLabelSpawn(j1Entity* enemy, uint damage)
 
 	SDL_Color c = {0, 0, 255, 255};  // label is created totally black 
 
-	iPoint pos(App->render->WorldToScreen(enemy->position.x, enemy->position.y));                                               // adjust this  
+	//iPoint pos(App->render->WorldToScreen(enemy->position.x, enemy->position.y));                                               // adjust this  
 
 
-	//iPoint pos(70, 70);
+	iPoint pos(70, 70);
 	App->gui->AddHitPointLabel(info, c, App->font->openSansBold36, pos, nullptr);    // big font for testing
 
 
