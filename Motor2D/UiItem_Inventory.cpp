5f811ad43453867bcd9bc0479f3b135fbf8755c9
Pol <36154612+PolRecasensSarra@users.noreply.github.com>
2019-04-16 23:24:13 +0200

    created inventory class base

---
+++ Motor2D/UiItem_Inventory.cpp
@@ -0,0 +1,6 @@
+#include "UiItem_Inventory.h"
+#include "j1Gui.h"
+
+//UiItem_Inventory::UiItem_Inventory()
+//{
+//}
---
+++ Motor2D/UiItem_Inventory.h
@@ -0,0 +1,26 @@
+#ifndef UIITEM_INVENTORY_H
+#define UIITEM_INVENTORY_H
+
+#include "UiItem.h"
+#include "p2Point.h"
+#include "j1Textures.h"
+#include <cstdint>
+
+
+class UiItem_Inventory : public UiItem
+{
+
+public:
+	//UiItem_Inventory();
+};
+
+
+
+
+
+
+
+
+
+
+#endif
