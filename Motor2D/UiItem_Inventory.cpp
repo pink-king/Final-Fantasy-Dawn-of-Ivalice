6ca5b8d5dce4f74ac15cbea875f4c8371d198d3e
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2019-04-17 14:11:39 +0200

    macros for invnetory grid

--- Motor2D/UiItem_Inventory.cpp
+++ Motor2D/UiItem_Inventory.cpp
@@ -1,6 +1,17 @@
 #include "UiItem_Inventory.h"
 #include "j1Gui.h"
 
-//UiItem_Inventory::UiItem_Inventory()
-//{
-//}
+UiItem_Inventory::UiItem_Inventory()
+{
+}
+
+
+
+void UiItem_Inventory::Draw(const float & dt)
+{
+
+
+
+}
+
+
--- Motor2D/UiItem_Inventory.h
+++ Motor2D/UiItem_Inventory.h
@@ -6,12 +6,22 @@
 #include "j1Textures.h"
 #include <cstdint>
 
+#define MAX_Weapons_plus_Equipment 15 
+#define MAX_Potions 3
+#define MAX_Equipped 3
+
+#define boxSize 48                   
+#define boxSeparation iPoint(16,13)
 
 class UiItem_Inventory : public UiItem
 {
 
 public:
-	//UiItem_Inventory();
+	UiItem_Inventory();
+
+
+	void Draw(const float& dt) override;
+
 };
 
 
