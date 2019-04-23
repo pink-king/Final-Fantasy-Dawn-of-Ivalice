8731c9e5482286fd6c3e02f51db5d27d1eb7dfcc
Sebi-Lopez <sebi8818@gmail.com>
2019-04-23 21:48:09 +0200

    Delete Stuff

--- Motor2D/j1EntityFactory.cpp
+++ Motor2D/j1EntityFactory.cpp
@@ -1606,17 +1606,3 @@ void j1EntityFactory::DoDescriptionComparison(LootEntity * lootItem)
 	
 
 }
-
-void j1EntityFactory::UnLoadLevelEntities()
-{
-	std::vector<j1Entity*>::reverse_iterator entitiesItem = entities.rbegin();
-
-	while (entitiesItem != entities.rend())
-	{
-		(*entitiesItem)->CleanUp();
-		RELEASE(*entitiesItem);
-		*entitiesItem = nullptr;
-		++entitiesItem;
-	}
-	entities.clear();
-}
