e154bcd4625ec8ee33a9500cdd5a3cff24cf5737
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2019-04-23 22:17:55 +0200

    reset clock spawn bools to prevent crash

--- Motor2D/Marche.cpp
+++ Motor2D/Marche.cpp
@@ -596,14 +596,17 @@ Marche::~Marche()
 		if (App->gui->spawnedClocks.Marche.special1)
 		{
 			myUIClocks.special1->to_delete = true;
+			App->gui->spawnedClocks.Marche.special1 = false; 
 		}
 		if (App->gui->spawnedClocks.Marche.special2)
 		{
 			myUIClocks.special2->to_delete = true;
+			App->gui->spawnedClocks.Marche.special2 = false;
 		}
 		if (App->gui->spawnedClocks.Marche.ulti)
 		{
 			myUIClocks.ulti->to_delete = true;
+			App->gui->spawnedClocks.Marche.ulti = false;
 		}
 	}
 	
