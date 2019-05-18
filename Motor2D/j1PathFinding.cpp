154eac73cefed4daf9fc4f7d0a2f89d83d4e076e
peterMcP <36275626+peterMcP@users.noreply.github.com>
2019-05-19 01:13:01 +0200

    fix pathfinding activate/deactivate tiles if the map is previously free

--- Motor2D/j1PathFinding.cpp
+++ Motor2D/j1PathFinding.cpp
@@ -775,8 +775,11 @@ void j1PathFinding::ActivateTile(const iPoint& tile)
 	if (CheckBoundaries(tile))
 	{
 		int id = GetIndexAt(tile);
-		if (map[id] != 0)
-			map[id] = 0;
+		if (map != NULL)
+		{
+			if (map[id] != 0)
+				map[id] = 0;
+		}
 	}
 }
 
@@ -785,7 +788,10 @@ void j1PathFinding::DeactivateTile(const iPoint& tile)
 	if (CheckBoundaries(tile))
 	{
 		int id = GetIndexAt(tile);
-		if (map[id] == 0)
-			map[id] = 1;
+		if (map != NULL)
+		{
+			if (map[id] == 0)
+				map[id] = 1;
+		}
 	}
 }
