f1fe74bf5f836425deb3b13fd0b3a580b8cf2015
GerardClotet <36155703+GerardClotet@users.noreply.github.com>
2019-05-19 19:20:53 +0200

    map clean

--- Game/maps/tilesetprueba.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="tilesetprueba" tilewidth="64" tileheight="64" spacing="1" margin="1" tilecount="63" columns="9">
- <image source="tilesetprueba.png" width="640" height="512"/>
-</tileset>
