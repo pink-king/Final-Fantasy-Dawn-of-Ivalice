5bc9da105d2a13fb507fcddfbab7d17081f3517b
peterMcP <36275626+peterMcP@users.noreply.github.com>
2019-05-20 01:32:02 +0200

    updated render, and switchet to windowed mode

--- Motor2D/j1Render.cpp
+++ Motor2D/j1Render.cpp
@@ -31,9 +31,13 @@ bool j1Render::Awake(pugi::xml_node& config)
 	{
 		flags |= SDL_RENDERER_PRESENTVSYNC;
 		LOG("Using vsync");
+		
 	}
 
 	renderer = SDL_CreateRenderer(App->win->window, -1, flags);
+	uint w, h;
+	App->win->GetWindowSize(w, h);
+	SDL_RenderSetLogicalSize(renderer, w, h);
 
 	if(renderer == NULL)
 	{
@@ -43,7 +47,7 @@ bool j1Render::Awake(pugi::xml_node& config)
 	else
 	{
 		camera = App->camera2D->GetCameraRectPtr();
-		App->camera2D->SetCameraRect({ 0,0, App->win->screen_surface->w,App->win->screen_surface->h});
+		App->camera2D->SetCameraRect({ 0,0, (int)w, (int)h});
 		/*camera.w = App->win->screen_surface->w;
 		camera.h = App->win->screen_surface->h;
 		camera.x = 0;
@@ -76,7 +80,7 @@ bool j1Render::Update(float dt)
 
 bool j1Render::PostUpdate()
 {
-	SDL_SetRenderDrawColor(renderer, background.r, background.g, background.g, background.a);
+	SDL_SetRenderDrawColor(renderer, background.r, background.g, background.b, background.a);
 	SDL_RenderPresent(renderer);
 	return true;
 }
