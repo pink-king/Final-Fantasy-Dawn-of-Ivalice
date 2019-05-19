3417fd9e77d4b05e36d505f17251d60b6a18c2cf
peterMcP <36275626+peterMcP@users.noreply.github.com>
2019-05-19 20:45:36 +0200

    fixed Ritz attack pivot offsets + spritesheet fix

--- Motor2D/Ritz.h
+++ Motor2D/Ritz.h
@@ -30,6 +30,7 @@ public:
 
 private:
 	fPoint dashPivotOffset[(int)facingDirection::MAX][4];
+	fPoint attackPivotOffset[(int)facingDirection::MAX];
 	Animation attack1[(int)facingDirection::MAX];
 	Animation tpAnim[(int)facingDirection::MAX];
 	SDL_Texture* attack1Tex = nullptr;
