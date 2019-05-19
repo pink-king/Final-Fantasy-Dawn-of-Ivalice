29574a9499cd46c725fdd9e290f649fbc5516a6a
Pol <36154612+PolRecasensSarra@users.noreply.github.com>
2019-05-19 17:52:47 +0200

    added to scene.cpp dialogueBox

--- Motor2D/j1Scene.cpp
+++ Motor2D/j1Scene.cpp
@@ -803,7 +803,11 @@ void j1Scene::LoadUiElement(UiItem* parent, pugi::xml_node node)
 		}
 		else if (charFlag == "Shara")
 		{
-			SharaIcon = App->gui->AddImage(position, &section, nullptr);
+			dialogueBox = App->gui->AddImage(position, &section, nullptr);
+		}
+		else if (charFlag == "bialogueBox")
+		{
+
 		}
 		else
 		{                                  // this is useless now
