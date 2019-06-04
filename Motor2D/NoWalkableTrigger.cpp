3fd2d4e0982bf46df90dd5a1ea2c13312da03c71
didaclis <36189205+didaclis@users.noreply.github.com>
2019-06-04 11:19:16 +0200

    fight boss crash solved

--- Motor2D/NoWalkableTrigger.cpp
+++ Motor2D/NoWalkableTrigger.cpp
@@ -38,7 +38,7 @@ bool NoWalkableTrigger::DoTriggerAction()
 			instantiationPoint.x -= 38;
 			App->entityFactory->CreateEntity(FLOWERBOSS, instantiationPoint.x, instantiationPoint.y, "flower_boss");
 			App->audio->PlayMusic("audio/music/bossmusic.ogg", -1);
-			App->entityFactory->CreateDialogTrigger(instantiationPoint.x, instantiationPoint.y, "BOSS", instantiationPoint, 10, false);     // boss dialog ready for lvl 2 interaction with boss
+			App->entityFactory->CreateDialogTrigger(instantiationPoint.x, instantiationPoint.y, "BOSS", { instantiationPoint.x - 2,instantiationPoint.y - 2}, 10, false);     // boss dialog ready for lvl 2 interaction with boss
 			if (!bossHall)
 				to_delete = true;
 		}
