5577a9f45206af71e9e7e3a34fc682b7168f3c19
peterMcP <36275626+peterMcP@users.noreply.github.com>
2019-06-08 21:51:16 +0200

    fixed ritz lastProjectile when swaps while remains a linked one protection

--- Motor2D/PlayerEntityManager.cpp
+++ Motor2D/PlayerEntityManager.cpp
@@ -113,9 +113,20 @@ bool PlayerEntityManager::Update(float dt)
 			UnlockPlayerInput();
 	}
 
+	// ALWAYS check ritz last projectile if ritz its not selected
+	// check if the player is not Ritz and she has a linked projectile protection
+	if (selectedCharacterEntity->character != characterName::RITZ)
+	{
+		ritz->CheckMyLastProjectile();
+	}
+
 	SwapInputChecker(); // checks gamepad "shoulders" triggers input
-	if(playerUpdateReady)
+
+	if (playerUpdateReady)
+	{
+		// update character
 		selectedCharacterEntity->Update(dt);
+	}
 	// update selected character position to its "manager" position
 	position = selectedCharacterEntity->position;
 	lastCharHeadingAngle = selectedCharacterEntity->GetLastHeadingAngle();
