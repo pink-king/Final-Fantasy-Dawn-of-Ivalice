de5e5755336614a5f2415242988b81586a9a167d
Sebi-Lopez <sebi8818@gmail.com>
2019-06-10 22:12:16 +0200

    Keybinds Debug Adjusted

--- Motor2D/j1Scene.cpp
+++ Motor2D/j1Scene.cpp
@@ -587,13 +587,13 @@ bool j1Scene::PreUpdate()
 	p = App->map->WorldToMap(p.x, p.y);
 
 
-	if (App->input->GetKey(SDL_SCANCODE_5) == KEY_DOWN)
+	if (App->input->GetKey(SDL_SCANCODE_F5) == KEY_DOWN)
 		App->win->SetScale(1);
 
-	if (App->input->GetKey(SDL_SCANCODE_6) == KEY_DOWN)
+	if (App->input->GetKey(SDL_SCANCODE_F6) == KEY_DOWN)
 		App->win->SetScale(2);
 
-	if (App->input->GetKey(SDL_SCANCODE_1) == KEY_DOWN) //if (App->input->GetKey(SDL_SCANCODE_F1) == KEY_DOWN)
+	if (App->input->GetKey(SDL_SCANCODE_F1) == KEY_DOWN) //if (App->input->GetKey(SDL_SCANCODE_F1) == KEY_DOWN)
 	{
 		App->map->ToggleDebugDraw();
 		debug = !debug;
@@ -779,6 +779,7 @@ bool j1Scene::Update(float dt)
 		iPoint coords = App->render->ScreenToWorld(x, y);
 		App->entityFactory->CreateLoot(coords.x, coords.y);
 	}
+
 	if (state == SceneState::STARTMENU)
 	{
 		result_volume = volume_bar->GetBarValue();
@@ -825,10 +826,10 @@ bool j1Scene::Update(float dt)
 	if (state == SceneState::LEVEL1 || state == SceneState::LEVEL2 || state == SceneState::LOBBY || state== SceneState::FIRINGRANGE)
 	{
 		//debug tp
-		if (App->input->GetKey(SDL_SCANCODE_F5) == KEY_DOWN)
+		if (App->input->GetKey(SDL_SCANCODE_5) == KEY_DOWN)
 			App->transitionManager->CreateFadeTransition(1.0,true,SceneState::LOBBY, Black, true, LvlPart::NO_PART);
 
-		if (App->input->GetKey(SDL_SCANCODE_F6) == KEY_DOWN)
+		if (App->input->GetKey(SDL_SCANCODE_6) == KEY_DOWN)
 			App->transitionManager->CreateFadeTransition(1.0,true, SceneState::FIRINGRANGE, Black, true, LvlPart::NO_PART);
 
 		if (App->input->GetKey(SDL_SCANCODE_7) == KEY_DOWN)
@@ -837,7 +838,7 @@ bool j1Scene::Update(float dt)
 		if (App->input->GetKey(SDL_SCANCODE_8) == KEY_DOWN)
 			App->transitionManager->CreateFadeTransition(1.0,true, SceneState::LEVEL1, Black, true, LvlPart::WAVES);
 
-		if (App->input->GetKey(SDL_SCANCODE_F9) == KEY_DOWN)
+		if (App->input->GetKey(SDL_SCANCODE_9) == KEY_DOWN)
 			App->transitionManager->CreateFadeTransition(1.0,true, SceneState::LEVEL2, Black, true, LvlPart::START);
 
 		if (App->input->GetKey(SDL_SCANCODE_0) == KEY_DOWN)
