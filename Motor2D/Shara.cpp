285fda7298308834023a00324ce70e89a15bf619
Sebi-Lopez <sebi8818@gmail.com>
2019-06-10 22:28:20 +0200

    Added cooldowns to shara abilities

--- Motor2D/Shara.cpp
+++ Motor2D/Shara.cpp
@@ -362,8 +362,8 @@ Shara::Shara(int posX, int posY):PlayerEntity(posX,posY)
 	// cooldown data test - TODO: import for each character its base cooldown in ms from xml
 	coolDownData.basic.cooldownTime = 0;
 	coolDownData.dodge.cooldownTime = 2000;
-	coolDownData.special1.cooldownTime = 500;
-	coolDownData.special2.cooldownTime = 3000;
+	coolDownData.special1.cooldownTime = 7000;
+	coolDownData.special2.cooldownTime = 5000;
 	coolDownData.ultimate.cooldownTime = 40000;
 	// set timers
 	SetCoolDownTimers();
